15828dc41e4e2424baaa1584c9a7a0d06817cd71
mrayush123456 <yk7129388@gmail.com>
2024-12-09 17:31:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -163,5 +163,4 @@ def send_facebook_message(access_token, recipient_id, message):
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host="0.0.0.0", port=5000, debug=True)
